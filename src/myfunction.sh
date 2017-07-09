1af40c54bd77ba88a2c3a94726be0262063b4703
Ivan <ivan@youzi.com.cn>
2017-07-09 17:14:19 +0800

    发布

---
+++ src/myfunction.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# 检测指令是否存在
+checkCommand()
+{
+    CommandList=$1
+    
+    if [[ $CommandList == "" ]]; then
+        echoError "checkCommand missing operand"
+        exit 1
+    fi
+    
+    for Command in $CommandList; do
+        if ( type -p $Command &> /dev/null ); then
+            if [ ! -L $(type -p $Command) ]; then
+                chmod 755 $(type -p $Command)
+            else
+                chmod 755 $(ls -l $(type -p $Command) | awk '{ print $NF }')
+            fi
+        else
+            echoError "$Command not found"
+            exit 1
+        fi
+    done
+}
+
+# 运行函数
+checkCommand ""
