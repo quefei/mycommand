154501ead985f1065c00877634ad5e2b942a88bc
Ivan <ivan@youzi.com.cn>
2017-07-09 20:00:57 +0800

    发布

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ if [ $(id -u) != "0" ]; then
 fi
 
 #
-CommandList="myfunction.sh mypath mydir mymake"
+CommandList="myfunction.sh mypath mydir mymake mydown"
 
 for Command in $CommandList; do
     CommandFile=/usr/local/bin/$Command
