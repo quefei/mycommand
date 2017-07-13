8d52619b0ffe50de01da4f4832f7d1be1563d260
Ivan <ivan@youzi.com.cn>
2017-07-13 10:08:11 +0800

    发布

--- src/myfunction.sh
+++ src/myfunction.sh
@@ -26,3 +26,49 @@ checkCommand()
 
 # 运行函数
 checkCommand quenong-md5 mypath mydir mymake mydown mytime
+
+# 查找 myoption.sh 文件
+myOption()
+{
+    MyOption=$(find ~/.[A-Za-z0-9_]*/ -name "myoption.sh" -type f | sed -n "1p")
+    
+    if [[ $MyOption != "" ]]; then
+        . $MyOption
+        grep "longOption()" $MyOption &> /dev/null || longOption() { :; }
+        grep "longArgument()" $MyOption &> /dev/null || longArgument() { :; }
+        grep "shortOption()" $MyOption &> /dev/null || shortOption() { :; }
+        grep "shortArgument()" $MyOption &> /dev/null || shortArgument() { :; }
+        grep "argumentEnd()" $MyOption &> /dev/null || argumentEnd() { :; }
+        grep "argumentTwo()" $MyOption &> /dev/null || argumentTwo() { :; }
+    else
+        longOption()
+        {
+            echoColor "Long Option:    $1" "31" "40"
+        }
+
+        longArgument()
+        {
+            echoColor "Long Argument:  $1" "35" "40"
+        }
+
+        shortOption()
+        {
+            echoColor "Short Option:   $1" "32" "40"
+        }
+
+        shortArgument()
+        {
+            echoColor "Short Argument: $1" "33" "40"
+        }
+
+        argumentEnd()
+        {
+            echoColor "Argument End:   $1" "34" "40"
+        }
+
+        argumentTwo()
+        {
+            echoColor "Argument Two:   $1" "36" "40"
+        }
+    fi
+}
