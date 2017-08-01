642f374280c843fd359f1759228e824952591fce
Ivan <ivan@youzi.com.cn>
2017-08-01 11:25:42 +0800

    发布

---
+++ backup/template.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ "$(id -u)" != "0" ]]; then
+        echo -n -e "\e[1m\e[40;31mError: \e[0m"
+        echo -e "\e[1m\e[40;37mmust run with root\e[0m"
+        exit 1
+fi
+
+SHELL_SCRIPT_LIST=""
+
+for SHELL_SCRIPT in $SHELL_SCRIPT_LIST; do
+        if [[]]; then
+        
+        else
+        
+        fi
+done
+
+#
+
