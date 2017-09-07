7f39e269d9a77a25b2fea7151d260e8f3ccbf9b3
Ivan <ivan@youzi.com.cn>
2017-09-07 09:45:42 +0800

    发布

--- install.sh
+++ install.sh
@@ -37,6 +37,12 @@ fi
 #       2.全局变量
 #
 ############################################################
+USR_COMMAND=
+USR_FILE=
+ROOT_COMMAND=
+ROOT_FILE=
+ROOT_COMMAND_DIR="/root/bin"
+RU_FILE_DIR="/root/myfile"
 
 ############################################################
 #
