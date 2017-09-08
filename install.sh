baff1fcaff7457ca66252f864343bd0793dbe42b
Ivan <ivan@youzi.com.cn>
2017-09-08 09:46:35 +0800

    发布

--- install.sh
+++ install.sh
@@ -39,7 +39,7 @@ fi
 ############################################################
 USR_COMMAND="myfunction.sh backup-centos backup-mysql backup-policy"
 USR_FILE=
-ROOT_COMMAND="config-centos install-lnmp"
+ROOT_COMMAND="config-centos install-lnmp read-lnmp"
 ROOT_FILE="epel.repo lnmp1.4.tar.gz"
 ROOT_DIR="/root/bin"
 FILE_DIR="/root/myfile"
