4f65d00af2d17f1e977b84df7ea17efa77d810ce
Ivan <ivan@youzi.com.cn>
2017-09-12 00:50:32 +0800

    发布

--- src/myfunction_option.sh
+++ src/myfunction_option.sh
@@ -62,3 +62,45 @@ back_option_number()
 #       
 #
 ############################################################
+letter_prefix_postfix()
+{
+        local LETTER=
+        local LETTER_LIST="A B C D E F G H I J K L M N O P Q R S T U V W X Y Z \
+                           a b c d e f g h i j k l m n o p q r s t u v w x y z \
+                           0 1 2 3 4 5 6 7 8 9"
+        
+        local PREFIX="$1"
+        local STRING="$2"
+        local POSTFIX="$3"
+        
+        for LETTER in ${LETTER_LIST}; do
+                STRING=$(echo "$STRING" | sed "s/${LETTER}/${PREFIX}${LETTER}${POSTFIX}/g")
+        done
+        
+        echo "$STRING"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
