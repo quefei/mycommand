3d97d9538ff516c806881393448218e8e8937f34
Ivan <ivan@youzi.com.cn>
2017-09-18 12:39:50 +0800

    发布

---
+++ uninstall_part.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+set -eu
+#set -x
+
+echo_color()
+{
+        echo -e "\e[1m\e[40;$2m$1\e[0m"
+}
+
+echo_error()
+{
+        echo_color "$1" "31"
+        exit 1
+}
+
+echo_warning()
+{
+        echo_color "$1" "33"
+        sleep 5
+}
+
+if [[ "$(id -u)" != "0" ]]; then
+        echo_error "Error: must run with root"
+fi
+
+# 全局变量
+FILE_LIST="install-cobbler
+           epel.repo
+           tftp settings dhcp.template pxedefault.template pxeprofile.template dhcpd.conf sample_end.ks
+           
+          "
+
+DIR_LIST=" /root /root/bin /root/myfile"
+
+DIR_LIST2="/root/bin /root/myfile"
+
+# 操作
+for DIR in ${DIR_LIST}; do
+        for FILE in ${FILE_LIST}; do
+                
+                FILE_PATH="${DIR}/${FILE}"
+                
+                if [[ -f "$FILE_PATH" ]]; then
+                        rm -rf "$FILE_PATH"
+                        echo "remove: ${FILE_PATH}"
+                fi
+                
+        done
+done
+
+#
+for DIR in ${DIR_LIST2}; do
+        
+        if [[ -d "$DIR" ]]; then
+                cd "$DIR"
+                
+                if [[ -z "$(ls -A)" ]]; then
+                        rm -rf "$DIR"
+                else
+                        echo_warning "Warning: ${DIR} is not empty"
+                fi
+        fi
+        
+done
